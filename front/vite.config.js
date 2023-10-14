1d74e81e1cd1de36f47492b4bca4c82a9d6e10cd
Kamil678 <kamilpigulak@interia.pl>
2023-10-14 12:24:42 +0200

    deploy to gh-pages

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PPISM/',
+  build: {
+    outDir: '../docs'
+  },
   plugins: [vue()],
 })
