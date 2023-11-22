a03d4a47666cc925324d75b2360c1c79af4e257f
Kamil Pigulak <kamil.pigulak@mediaflex.pl>
2023-11-22 08:32:59 +0100

    add product

--- src/pages/AddTechnologicalCard.vue
+++ src/pages/AddTechnologicalCard.vue
@@ -12,15 +12,37 @@
         </div>
         <div class="product-name">
           <div class="label">Nazwa montowanego wyrobu</div>
-          <div class="data"></div>
+          <div class="data">Reduktor walcowy</div>
         </div>
         <div class="picture-number">
           <div class="label">Numer rysunku złożeniowego</div>
-          <div class="number"></div>
+          <div class="data">6</div>
         </div>
         <div class="size-series">
           <div class="label">Wielkość serii</div>
-          <div class="data"></div>
+          <div class="data">100</div>
+        </div>
+      </div>
+      <div class="description-rows-wrap">
+        <div class="operation-number">
+          Nr operacji
+        </div>
+        <div class="operation-content">
+          Treść operacji
+        </div>
+        <div class="position">
+          Stanowisko
+        </div>
+        <div class="position-symbol">
+          Symbol stanowiska
+        </div>
+        <div class="time-norms">
+          <div>Normy czasu</div>
+          <div class="specific-times">
+            <div>tpz</div>
+            <div>tj</div>
+            <div>Nt</div>
+          </div>
         </div>
       </div>
     </div>
@@ -60,6 +82,38 @@
       width: calc(100% / 5);
       text-align: center;
       border-right: 2px solid #000;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: 500;
+
+      .data {
+        font-weight: 400;
+      }
+    }
+
+    .title {
+      font-size: 20px;
+    }
+
+    .product-name,
+    .picture-number,
+    .size-series {
+      div {
+        min-height: 50%;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+
+    .title {
+      width: 50%;
+    }
+
+    .size-series {
+      border-right: none;
     }
 
     .label {
@@ -75,6 +129,70 @@
       .label {
         min-height: 50%;
         border-bottom: 2px solid #000;
+      }
+    }
+  }
+
+  .description-rows-wrap {
+    min-height: 100px;
+    border-bottom: 2px solid #000;
+    display: flex;
+    background-color: $dark-grey;
+
+    &>div {
+      min-height: 100%;
+      width: calc(100% / 5);
+      text-align: center;
+      border-right: 2px solid #000;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: 500;
+
+      .data {
+        font-weight: 400;
+      }
+    }
+
+    .operation-number {
+      width: 7%;
+    }
+
+    .operation-content {
+      flex-grow: 1;
+    }
+
+    .time-norms {
+      display: flex;
+      flex-direction: column;
+      border-right: none;
+
+      &>div {
+        min-height: 50%;
+      }
+
+      div:first-child {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .specific-times {
+        border-top: 2px solid #000;
+        display: flex;
+        width: 100%;
+
+        div {
+          width: calc(100% / 3);
+          border-right: 2px solid #000;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        div:last-child {
+          border-right: none !important;
+        }
       }
     }
   }
